e5f5998fc714b87241d567f7ca3d6e72f450823b
ParkerCatz <tksm.webdesign09@gmail.com>
2021-04-08 22:12:55 +0900

    add: Taskコンポーネントの追加

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,45 @@
 <template>
   <div class="container">
     <Header title="Task Tracker" />
+    <Tasks :tasks="tasks" />
   </div>
 </template>
 
 <script>
 import Header from './components/Header'
+import Tasks from './components/Tasks'
 export default {
   name: 'App',
   components: {
     Header,
+    Tasks,
+  },
+  data() {
+    return {
+      tasks: [],
+    }
+  },
+  created() {
+    this.tasks = [
+      {
+        id: 1,
+        text: 'AAAAAA',
+        day: '04/01',
+        reminder: true,
+      },
+      {
+        id: 2,
+        text: 'BBBBBB',
+        day: '05/01',
+        reminder: true,
+      },
+      {
+        id: 3,
+        text: 'CCCCCC',
+        day: '06/01',
+        reminder: false,
+      },
+    ]
   },
 }
 </script>
